fa1db8bd4b6ac4bf2544e8a078621785bc574947
pim <pimmiesz.p@gmail.com>
2018-05-11 15:51:08 +0700

    css

--- need2fix/app/models.py
+++ need2fix/app/models.py
@@ -31,7 +31,7 @@ class Task(models.Model):
     building = models.CharField(max_length=32)
     floor = models.CharField(max_length=32)
     room = models.CharField(max_length=32, null=True, blank=True)
-    etc = models.CharField(max_length=32, null=False, blank=False)
+    etc = models.CharField(max_length=32, null=True, blank=False)
     create_date = models.DateTimeField(auto_now_add=True)
     done_date = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=1, blank=True, choices=STATUS_CHOICES)
